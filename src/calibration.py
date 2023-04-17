30b436789a4f02307948c21bc77fcc7d5fc4161a
Nicolas LESQUOY <nicolas.lesquoy@gmail.com>
2023-04-17 20:57:27 +0200

    Update to new version of calibration

--- src/calibration.py
+++ src/calibration.py
@@ -1,38 +1,87 @@
+# Standard librairies
 import os
 import json
+from functools import reduce
+import operator
+from pathlib import Path
+import logging
+# Make this import robust for Python 3.11
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Third party librairies
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from matplotlib.patches import Circle
+from matplotlib import transforms
 from PIL import Image
 import numpy as np
 import pandas as pd
-from functools import reduce
-import operator
-# import cv2
 
 class Calibration:
-    # TODO Move path to config
-    path_to_reference = r"/home/nlesquoy/Documents/Cours/EAEP-209/TP MAE 2022/Calibration/"
-    path_to_calibration_output = r"/home/nlesquoy/Documents/Cours/post-processing/src/calibration.json"
-    path_to_intermediary = r"/home/nlesquoy/Documents/Cours/post-processing/src/intermediary.csv"
+    # TODO Move to 'pathlib' librairy + implement logging in all functions
+    with open("config.toml","rb") as f:
+        config = tomllib.load(f)
 
-    def calibration(path_to_image):
-        fig = plt.figure()
-        img = np.asarray(Image.open(path_to_image))
-        name = os.path.basename(path_to_image) 
-        plt.imshow(img)
-        def onclick(event):
-            with open(Calibration.path_to_intermediary,"a") as f:
-                f.write(f"{name},{event.xdata},{event.ydata}" + "\n")
-                f.close()
-            print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %(event.button, event.x, event.y, event.xdata, event.ydata))
-            plt.plot(event.xdata, event.ydata, '.',c='b')
-            fig.canvas.draw()
-        cid = fig.canvas.mpl_connect('button_press_event', onclick)
-        plt.show()
+    send_click_to_log = config["parameters"]["send_onclick_to_log"]
+
+    path_to_calibration = Path(config["paths"]["path_to_calibration"])
+    path_to_camera_calibration = Path(config["paths"]["path_to_camera_calibration"])
+    path_to_debug = Path(config["paths"]["path_to_debug"])
+    path_to_logs = Path(config["paths"]["path_to_logs"])
+    path_to_calibration_log = path_to_logs / "calibration.log"
+    path_to_intermediary = path_to_debug / "intermediary.csv"
+
+    logging.basicConfig(filename=path_to_calibration_log,format='%(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S',level=logging.DEBUG)
+
+    def clear_calibration_file(path_to_calibration_file: Path) -> bool:
+        """Remove the calibration file if it exists."""
+        if os.path.exists(path_to_calibration_file):
+            os.remove(path_to_calibration_file)
+            return True
+        else:
+            return False
+    
+    def clear_log_file(path_to_log_file: Path) -> bool:
+        """Remove the calibration file if it exists."""
+        if os.path.exists(path_to_log_file):
+            os.remove(path_to_log_file)
+            return True
+        else:
+            return False
+
+    def calibration(path_to_image: Path,rotation: int = 180):
+        """
+        This function is used to calibrate the camera.
+        It takes a path to an image as input and returns a dictionary with the coordinates of the corners of the image.
+        """
+        try:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            img = np.asarray(Image.open(path_to_image))
+            name = os.path.basename(path_to_image)
+            print(name)
+            tr = transforms.Affine2D().rotate_deg(rotation)
+            ax.imshow(img,transforms=tr)
+            def onclick(event):
+                with open(Calibration.path_to_intermediary,"a") as f:
+                    f.write(f"{name},{event.xdata},{event.ydata}" + "\n")
+                    f.close()
+                # Logging to calibration log file
+                logging.debug('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %(event.button, event.x, event.y, event.xdata, event.ydata))
+                plt.plot(event.xdata, event.ydata, '.',c='b')
+                fig.canvas.draw()
+            cid = fig.canvas.mpl_connect('button_press_event', onclick)
+            plt.show()
+        except Exception as e:
+            logging.exception("Exception occurred")
         return 0
     
     def iterate(path_to_folder):
+        """
+        Iterate over all elements in the folder
+        TODO Migrate to 'pathlib' module instead of os
+        """
         for filename in os.listdir(path_to_folder):
             f = os.path.join(path_to_folder, filename)
             # checking if it is a file
@@ -41,6 +90,9 @@ class Calibration:
         return None
     
     def create_reference_file(path_to_calibration_file):
+        """
+        Create a reference file for the calibration
+        """
         df = pd.read_csv(Calibration.path_to_intermediary,sep=",",header=None,index_col=0)
         result_dict = {}
         for image,data in df.groupby(0):
@@ -51,6 +103,7 @@ class Calibration:
         return result_dict
 
     def order_corner_clockwise(points_dict):
+        """Orders all the found corners in clockwise order to be able to use them in the perspective transform with consistent results."""
         new_dict = {}
         for key in points_dict:
             coords = points_dict[key]
@@ -60,36 +113,45 @@ class Calibration:
         return new_dict
     
     def create_json(result_dict):
+        """Create a json file with the coordinates of the corners of the image."""
         with open(Calibration.path_to_calibration_output,"w") as f:
             json.dump(result_dict,f,indent=4)
             f.close()
         
     def create_sanity_check(result_dict):
-        colors = cm.rainbow(np.linspace(0, 0.01, len(result_dict)))
+        """Create a sanity check image of all founds coreners"""
         for key in result_dict:
             points = np.array(list(map(list, result_dict[key])))
-            # print(points)
             x_points = points[:,[0]]
-            # print(x_points)
             y_points = points[:,[1]]
-            # print(y_points)
             plt.scatter(x_points,y_points,marker=".",label=f"{key}")
         plt.legend(loc="best",fontsize="small")
         plt.xlabel("$x$")
         plt.ylabel("$y$")
         plt.savefig("test/sanity.jpg")
+    
+    def driver():
+        """Driver function for the calibration process."""
+        try:
+            # Clearing the calibration file
+            Calibration.clear_calibration_file(Calibration.path_to_intermediary)
+            # Clearing the log file
+            Calibration.clear_log_file(Calibration.path_to_calibration_log)
+            # Iterating over all images
+            Calibration.iterate(Calibration.path_to_calibration)
+            # Creating the reference file
+            result = Calibration.create_reference_file("truc")
+            # Ordering the corners in clockwise order
+            result_ordered = Calibration.order_corner_clockwise(result)
+            # Creating the json file
+            Calibration.create_json(result_ordered)
+            # Creating a sanity check image
+            Calibration.create_sanity_check(result_ordered)
+            return 0
+        except Exception as e:
+            logging.exception("Exception occurred")
+            return 1
         
 
-result = Calibration.create_reference_file("truc")
-result_ordered = Calibration.order_corner_clockwise(result)
-Calibration.create_json(result_ordered)
-# # Sanity check
-# Calibration.create_sanity_check(result_ordered)
-# for key in result_ordered:
-#     for i in range(len(result_ordered[key])):
-#         if int(key) % 2 == 0:
-#             plt.plot(result_ordered[key][i][0],result_ordered[key][i][1],".",c="b")
-#         else:
-#             plt.plot(result_ordered[key][i][0],result_ordered[key][i][1],".",c="r")
-#         plt.annotate(str(f"({i}/{key})"),(result_ordered[key][i][0],result_ordered[key][i][1]))
-# plt.show()
+if __name__ == "__main__":
+    Calibration.driver()
